fcb9fd89034ea3f43876299ac227248be990bc8a
Victor Clausen <100559970+Minirz@users.noreply.github.com>
2022-08-10 12:37:59 +0200

    js and json added

---
+++ src/script.js
@@ -0,0 +1,34 @@
+fetch("https://api.github.com/users/minirz")
+  .then((reponse) => reponse.json())
+  .then((profil) => {
+    console.log(profil.avatar_url);
+
+    let imageContainer = document.querySelector(".profile-image");
+    let imageText = document.querySelector(".profile-text");
+
+    imageContainer.src = profil.avatar_url;
+
+    // imageText.innerHTML = `<b>${profil.login} er mit <i>navn</i></b>`;
+    imageText.insertAdjacentHTML(
+      "beforebegin",
+      `<b>${profil.login} er mit <i>navn</i></b>`
+    );
+
+    // imageText.textContent = profil.login;
+
+    // imageText.innerHTML = "<b>" + profil.login + "</b>"
+    // "<b>" + profil.login + "</b>";
+    // imageText.textContent = profil.login;
+
+    console.log("TEST", imageContainer.src, imageContainer.id);
+  });
+
+function open() {
+  console.log("open");
+}
+open();
+
+const openMe = () => {
+  console.log("Open me");
+};
+openMe();
